a0824f9575de415008d61f329963353485af064c
schang1146 <sammychang2185@gmail.com>
2019-06-13 01:27:43 -0700

    started to add update and delete options

--- friends/src/App.js
+++ friends/src/App.js
@@ -36,6 +36,34 @@ class App extends React.Component {
         this.setState({ errorMsg: err });
       });
   }
+
+  deleteFriend = (e, friend) => {
+    e.preventDefault();
+    axios
+      .delete('http://localhost:5000/friends', friend)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        });
+      })
+      .catch(err => {
+        this.setState({ errorMsg: err });
+      });
+  }
+
+  updateFriend = (e, friend) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:5000/friends/${friend.id}`, friend)
+      .then(response => {
+        this.setState({
+          friends: response.data
+        });
+      })
+      .catch(err => {
+        this.setState({ errorMsg: err });
+      });
+  }
   
   render() {
     return (
--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 export default class Friend extends React.Component {
     render() {
@@ -7,6 +8,10 @@ export default class Friend extends React.Component {
                 <h2>{this.props.friend.name}</h2>
                 <p>Age: {this.props.friend.age}</p>
                 <p>Email: {this.props.friend.email}</p>
+                <div className='friend-options'>
+                    <Button color='info'>Edit</Button>{' '}
+                    <Button color='danger'>Delete</Button>
+                </div>
             </div>
         )
     }
